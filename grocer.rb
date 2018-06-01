94ffd5d80f811b6826622c9077c14c65f7343213
slowbeam <sedwardscode@gmail.com>
2018-06-01 12:55:54 -0400

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,49 @@
+require "pry"
 def consolidate_cart(cart)
-  # code here
-end
+  new_cart = {}
+  cart.each do |x|
+    x.each do |food, info|
+      info[:count] = cart.count(x)
+    end
+    new_cart.merge!(x)
+    end
+new_cart
+  end
+
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    if cart.has_key?(coupon[:item]) && cart[coupon[:item]][:count] >= coupon[:num]
+      item_withcoupon = coupon[:item] + " W/COUPON"
+    if cart.has_key?(item_withcoupon)
+      cart[item_withcoupon][:count] += 1
+    else
+      cart[item_withcoupon] = {price: coupon[:cost], clearance: cart[coupon[:item]][:clearance], count: 1}
+    end
+    cart[coupon[:item]][:count] -= coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, info|
+    if info[:clearance] == true
+      info[:price] = info[:price] - (info[:price] * 0.2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  total = 0
+  cart.each do |item, info|
+    total += info[:price] * info[:count]
+  end
+  if total >= 100
+    total = total * 0.9
+  end
+  total
 end
